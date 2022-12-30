9751982ff851b7e4a60ffb76f28b543c68e90157
SONSAZANG <kekekee257@gmail.com>
2022-12-30 18:33:55 +0900

    화면 사이즈 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
+import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
@@ -36,68 +37,120 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int count = 0;
+  int bibleCount = 0;
+  int bgTitlesCount = 0;
+
   final List<Map<String, String>> bibles = [
     {
-      'titleA': "창세기 1장 1절",
-      'descriptingA': "태초에 하나님이 천지를 창조하시니라",
+      'title': "창세기 1장 1절",
+      'description': "태초에 하나님이\n천지를 창조하시니라",
     },
     {
-      'titleA': "창세기 1장 2절",
-      'descriptingA': "땅이혼돈하고 공허하며 흑암이 깊음 위에 있고 하나님의 영은 수면 위에 운행하시니라",
+      'title': "창세기 1장 2절",
+      'description': "땅이혼돈하고 공허하며\n흑암이 깊음 위에 있고\n하나님의 영은 수면 위에 운행하시니라",
+    },
+    {
+      'title': "히브리서 4:16",
+      'description':
+          "그러므로 우리가 긍휼하심을 받고\n때를 따라 돕는 은혜를 얻기 위하여\n은혜의 보좌 앞에\n담대히 나아갈 것이니라",
+    },
+    {
+      'title': "누가복음 2:20",
+      'description':
+          "목자가 자기들에게\n이른던 바와 같이 듣고 본\n그 모든 것을 인하여\n하나님께 영광을 돌리고\n찬송하며 돌아가니라",
+    },
+    {
+      'title': "시편 119:10",
+      'description': "내가 전심으로\n주를 찾았사오니\n주의 계명에서\n떠나지 말게 하소서",
     },
   ];
-  final List<String> BGTitles = [
+  final List<String> bgTitles = [
     "images/BG01.png",
     "images/BG02.png",
     "images/BG03.png",
     "images/BG04.png",
     "images/BG05.png",
   ];
-  String titleA = "";
-  String descriptingA = "";
-  String BGTitle = "images/BG01.png";
+  String title = "";
+  String description = "";
+  String bgTitle = "images/BG01.png";
   void changeTitle() {
     setState(() {
-      titleA = bibles[count].values.first;
-      descriptingA = bibles[count].values.last;
-      count++;
-      BGTitle = BGTitles[Random().nextInt(6)];
+      bibleCount = Random().nextInt(bibles.length);
+      bgTitlesCount = Random().nextInt(bgTitles.length);
+      title = bibles[bibleCount].values.first;
+      description = bibles[bibleCount].values.last;
+      bgTitle = bgTitles[bgTitlesCount];
     });
+  }
+
+  void loadJson() {
+    var list_data = [
+      {"verseid": "창세기 1장 1절", "verse": "태초에 하나님이\\n천지를 창조하시니라"},
+      {
+        "verseid": "창세기 1장 2절",
+        "verse": "땅이혼돈하고 공허하며\\n흑암이 깊음 위에 있고\\n하나님의 영은 수면 위에 운행하시니라"
+      },
+      {
+        "verseid": "히브리서 4:16",
+        "verse":
+            "그러므로 우리가 긍휼하심을 받고\\n때를 따라 돕는 은혜를 얻기 위하여\\n은혜의 보좌 앞에\\n담대히 나아갈 것이니라"
+      },
+      {
+        "verseid": "누가복음 2:20",
+        "verse":
+            "목자가 자기들에게\\n이른던 바와 같이 듣고 본\\n그 모든 것을 인하여\\n하나님께 영광을 돌리고\\n찬송하며 돌아가니라"
+      },
+      {
+        "verseid": "시편 119:10",
+        "verse": "내가 전심으로\\n주를 찾았사오니\\n주의 계명에서\\n떠나지 말게 하소서"
+      }
+    ];
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        constraints: BoxConstraints.expand(),
+        constraints: const BoxConstraints(minWidth: 380, minHeight: 800),
         decoration: BoxDecoration(
-          image: DecorationImage(image: AssetImage(BGTitle), fit: BoxFit.fill),
+          image: DecorationImage(image: AssetImage(bgTitle), fit: BoxFit.fill),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              '말씀 뽑기',
-              style: GoogleFonts.getFont('Nanum Brush Script',
-                  fontSize: 50,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                '말씀 뽑기',
+                style: GoogleFonts.getFont('Nanum Brush Script',
+                    fontSize: 50,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w400),
+              ),
+              Text(
+                title,
+                textAlign: TextAlign.center,
+                style: GoogleFonts.getFont('Nanum Brush Script',
+                    fontSize: 30, color: Colors.black),
+              ),
+              const SizedBox(
+                width: 30,
+                height: 30,
+              ),
+              Text(
+                description,
+                textAlign: TextAlign.center,
+                style: GoogleFonts.getFont(
+                  'Nanum Brush Script',
+                  fontSize: 25,
                   color: Colors.black,
-                  fontWeight: FontWeight.w400),
-            ),
-            Text(
-              titleA,
-              style: GoogleFonts.getFont('Nanum Brush Script',
-                  fontSize: 30, color: Colors.black),
-            ),
-            Text(
-              descriptingA,
-              style: GoogleFonts.getFont('Nanum Brush Script',
-                  fontSize: 30, color: Colors.black),
-            ),
-            ElevatedButton(
-                onPressed: () => {changeTitle()}, child: Text('말씀 뽑기'))
-          ],
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () => {changeTitle()}, child: const Text('말씀 뽑기'))
+            ],
+          ),
         ),
       ),
     );
